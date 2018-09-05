d52b2433a46bc725ee4d1ce0525e4bba985ed740
nikknez333 <32225918+nikknez333@users.noreply.github.com>
2018-09-05 10:53:08 +0200

    IzmeneKreiranjaCommit

--- WEBProjekatMVC/Taxi_MVC/Taxi_MVC/Controllers/TaxiController.cs
+++ WEBProjekatMVC/Taxi_MVC/Taxi_MVC/Controllers/TaxiController.cs
@@ -32,7 +32,7 @@ namespace Taxi_MVC.Controllers
         {
             StreamWriter writer3 = new StreamWriter(path + "packages/Korisnici.txt");
 
-            foreach(KeyValuePair<string, Korisnik> kv in getKorisnik)
+            foreach (KeyValuePair<string, Korisnik> kv in getKorisnik)
             {
                 writer3.WriteLine(kv.Value.KorisnickoIme + " " + kv.Value.Lozinka + " " + kv.Value.Ime + " " + kv.Value.Prezime + " " + kv.Value.Pol + " " + kv.Value.Email +
                 " " + kv.Value.JMBG + " " + kv.Value.Telefon);
@@ -95,7 +95,7 @@ namespace Taxi_MVC.Controllers
         public ActionResult Registration(Korisnik k)
         {
             k.Uloga = EUloga.MUSTERIJA;
-            if(getKorisnik.ContainsKey(k.KorisnickoIme) || getAdministratori.ContainsKey(k.KorisnickoIme) || getVozaci.ContainsKey(k.KorisnickoIme))
+            if (getKorisnik.ContainsKey(k.KorisnickoIme) || getAdministratori.ContainsKey(k.KorisnickoIme) || getVozaci.ContainsKey(k.KorisnickoIme))
             {
                 ViewBag.korisnik = k;
                 return View("RegistrationError");
@@ -114,21 +114,21 @@ namespace Taxi_MVC.Controllers
         {
             int i = 0;
 
-            if(!getKorisnik.ContainsKey(korisnickoIme))
+            if (!getKorisnik.ContainsKey(korisnickoIme))
             {
-                if(getAdministratori.ContainsKey(korisnickoIme))
+                if (getAdministratori.ContainsKey(korisnickoIme))
                 {
-                    if(!getAdministratori[korisnickoIme].Lozinka.Equals(lozinka))
+                    if (!getAdministratori[korisnickoIme].Lozinka.Equals(lozinka))
                     {
                         return View("LozinkaError");
                     }
                     Session["Ulogovan"] = getAdministratori[korisnickoIme];
                     i = 0;
-                    foreach(KeyValuePair<string, Voznja> kv in getVoznje)
+                    foreach (KeyValuePair<string, Voznja> kv in getVoznje)
                     {
                         try
                         {
-                            if(kv.Value.Dispecer.KorisnickoIme.Equals(getAdministratori[korisnickoIme].KorisnickoIme))
+                            if (kv.Value.Dispecer.KorisnickoIme.Equals(getAdministratori[korisnickoIme].KorisnickoIme))
                             {
                                 i++;
                             }
@@ -141,19 +141,19 @@ namespace Taxi_MVC.Controllers
                     ViewBag.broj = i;
                     return View("HomepageAdministrator");
                 }
-                if(getVozaci.ContainsKey(korisnickoIme))
+                if (getVozaci.ContainsKey(korisnickoIme))
                 {
-                    if(!getVozaci[korisnickoIme].Lozinka.Equals(lozinka))
+                    if (!getVozaci[korisnickoIme].Lozinka.Equals(lozinka))
                     {
                         return View("LozinkaError");
                     }
                     Session["Ulogovan"] = getVozaci[korisnickoIme];
                     i = 0;
-                    foreach(KeyValuePair<string,Voznja> kv in getVoznje)
+                    foreach (KeyValuePair<string, Voznja> kv in getVoznje)
                     {
                         try
                         {
-                            if(kv.Value.Vozac.KorisnickoIme.Equals((((Korisnik)Session["Ulogovan"]).KorisnickoIme)))
+                            if (kv.Value.Vozac.KorisnickoIme.Equals((((Korisnik)Session["Ulogovan"]).KorisnickoIme)))
                             {
                                 i++;
                             }
@@ -170,18 +170,18 @@ namespace Taxi_MVC.Controllers
                 return View("KorisnickoImeError");
             }
 
-            if(!getKorisnik[korisnickoIme].Lozinka.Equals(lozinka))
+            if (!getKorisnik[korisnickoIme].Lozinka.Equals(lozinka))
             {
                 return View("LozinkaError");
             }
 
             Session["Ulogovan"] = getKorisnik[korisnickoIme];
             i = 0;
-            foreach(KeyValuePair<string, Voznja> kv in getVoznje)
+            foreach (KeyValuePair<string, Voznja> kv in getVoznje)
             {
                 try
                 {
-                    if(kv.Value.Musterija.KorisnickoIme.Equals(getKorisnik[korisnickoIme].KorisnickoIme))
+                    if (kv.Value.Musterija.KorisnickoIme.Equals(getKorisnik[korisnickoIme].KorisnickoIme))
                     {
                         i++;
                     }
@@ -215,6 +215,199 @@ namespace Taxi_MVC.Controllers
             Session["Ulogovan"] = null;
             return View("Index");
         }
+
+        public ActionResult Izmeni(Korisnik k)
+        {
+            if (!((Korisnik)Session["Ulogovan"]).KorisnickoIme.Equals(k.KorisnickoIme) && (getKorisnik.ContainsKey(k.KorisnickoIme) || getAdministratori.ContainsKey(k.KorisnickoIme) || getVozaci.ContainsKey(k.KorisnickoIme)))
+            {
+                ViewBag.korisnik = k;
+                return View("RegistrationError2");
+            }
+            if (!((Korisnik)Session["Ulogovan"]).KorisnickoIme.Equals(k.KorisnickoIme))
+            {
+                getKorisnik.Remove(((Korisnik)Session["Ulogovan"]).KorisnickoIme);
+            }
+            k.Uloga = EUloga.MUSTERIJA;
+            getKorisnik[k.KorisnickoIme] = k;
+            Session["Ulogovan"] = getKorisnik[k.KorisnickoIme];
+            int i = 0;
+            foreach (KeyValuePair<string, Voznja> kv in getVoznje)
+            {
+                if (kv.Value.Musterija.KorisnickoIme.Equals((((Korisnik)Session["Ulogovan"]).KorisnickoIme)))
+                {
+                    i++;
+                }
+            }
+            IspisKorisnika();
+            ViewBag.broj = i;
+            return View("HomepageMusterija");
+        }
+
+        public ActionResult IzmeniVozac(Vozac voz, Automobil auto, Lokacija lok, Adresa adr)
+        {
+            auto.VozacAuto = voz;
+            voz.AutoVozac = auto;
+            lok.AdresaLok = adr;
+            voz.LokVozac = lok;
+            voz.Zauzet = false;
+            voz.Uloga = EUloga.VOZAC;
+
+            if (!((Vozac)Session["Ulogovan"]).KorisnickoIme.Equals(voz.KorisnickoIme) && (getKorisnik.ContainsKey(voz.KorisnickoIme) || getAdministratori.ContainsKey(voz.KorisnickoIme) || getVozaci.ContainsKey(voz.KorisnickoIme)))
+            {
+                ViewBag.vozac = voz;
+                return View("IzmenaVozacError");
+            }
+            if (!((Vozac)Session["Ulogovan"]).KorisnickoIme.Equals(voz.KorisnickoIme))
+            {
+                getVozaci.Remove(((Vozac)Session["Ulogovan"]).KorisnickoIme);
+
+            }
+            getVozaci[voz.KorisnickoIme] = voz;
+            Session["Ulogovan"] = getVozaci[voz.KorisnickoIme];
+            int i = 0;
+
+            foreach (KeyValuePair<string, Voznja> kv in getVoznje)
+            {
+                try
+                {
+                    if (kv.Value.Vozac.KorisnickoIme.Equals((((Korisnik)Session["Ulogovan"]).KorisnickoIme)))
+                    {
+                        i++;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            IspisVozaca();
+            ViewBag.broj = i;
+            return View("HomepageVozac");
+        }
+
+        public ActionResult IzmeniAdmin(Korisnik k)
+        {
+            if (!((Korisnik)Session["Ulogovan"]).KorisnickoIme.Equals(k.KorisnickoIme) && (getKorisnik.ContainsKey(k.KorisnickoIme) || getAdministratori.ContainsKey(k.KorisnickoIme) || getVozaci.ContainsKey(k.KorisnickoIme)))
+            {
+                ViewBag.korisnik = k;
+                return View("RegistrationError");
+            }
+            if (!((Korisnik)Session["Ulogovan"]).KorisnickoIme.Equals(k.KorisnickoIme))
+            {
+                getAdministratori.Remove(((Korisnik)Session["Ulogovan"]).KorisnickoIme);
+            }
+            k.Uloga = EUloga.DISPECER;
+            getAdministratori[k.KorisnickoIme] = k;
+            Session["Ulogovan"] = getAdministratori[k.KorisnickoIme];
+            int i = 0;
+            foreach (KeyValuePair<string, Voznja> kv in getVoznje)
+            {
+                try
+                {
+                    if (kv.Value.Dispecer.KorisnickoIme.Equals((((Korisnik)Session["Ulogovan"]).KorisnickoIme)))
+                    {
+                        i++;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            ViewBag.broj = i;
+            return View("HomepageAdministrator");
+        }
+
+        public ActionResult GoToKreiranjeVozaca()
+        {
+            return View("KreiranjeVozaca");
+        }
+
+        [HttpPost]
+        public ActionResult KreiranjeVozaca(Vozac voz, Automobil auto, Lokacija lok, Adresa adr)
+        {
+            voz.AutoVozac = auto;
+            auto.VozacAuto = voz;
+            lok.AdresaLok = adr;
+            voz.Zauzet = false;
+            voz.Uloga = EUloga.VOZAC;
+
+            if(getKorisnik.ContainsKey(voz.KorisnickoIme) || getAdministratori.ContainsKey(voz.KorisnickoIme) || getVozaci.ContainsKey(voz.KorisnickoIme))
+            {
+                ViewBag.vozac = voz;
+                return View("KreiranjeVozacaError");
+            }
+            getVozaci.Add(voz.KorisnickoIme, voz);
+            int i = 0;
+            foreach(KeyValuePair<string,Voznja> kv in getVoznje)
+            {
+                try
+                {
+                    if(kv.Value.Dispecer.KorisnickoIme.Equals(((Korisnik)Session["Ulogovan"]).KorisnickoIme))
+                    {
+                        i++;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            IspisVozaca();
+            ViewBag.broj = i;
+            return View("HomepageAdministrator");
+        }
+
+        public ActionResult PromeniLokaciju()
+        {
+            return View("PromenaLokacije");
+        }
+        [HttpPost]
+        public ActionResult PromenaLokacije(string log,Lokacija lok, Adresa adr)
+        {
+            lok.AdresaLok = adr;
+            getVozaci[log].LokVozac = lok;
+            Session["Ulogovan"] = getVozaci[log];
+            int i = 0;
+            foreach(KeyValuePair<string,Voznja> kv in getVoznje)
+            {
+                try
+                {
+                    if(kv.Value.Vozac.KorisnickoIme.Equals(((Korisnik)Session["Ulogovan"]).KorisnickoIme))
+                    {
+                        i++;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            ViewBag.broj = i;
+            return View("HomepageVozac");
+        }
+
+        public ActionResult MusterijaZahtev()
+        {
+            return View("MusterijaZahtev");
+        }
+
+        [HttpPost]
+        public ActionResult KreiranjeVoznje(Lokacija lok, Adresa adr, ETipAutomobila tip)
+        {
+            lok.AdresaLok = adr;
+            Voznja v = new Voznja();
+            v.LokacijaDolaskaTaxi = lok;
+            v.DatumVremePorudzbine = DateTime.Now;
+            v.ZeljeniTipAuto = tip;
+            v.StatusVoznje = EStatus.KREIRANA;
+            v.Musterija = (Korisnik)Session["Ulogovan"];
+            v.Vozac = new Vozac();
+            v.KomentarNaVoznje = new Komentar();
+            getVoznje.Add(v.DatumVremePorudzbine.ToString(), v);
+            return View("PregledSvihVoznjiMusterije");
+        }
+
 
     }
 }
